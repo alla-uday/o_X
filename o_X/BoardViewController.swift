6445b383ff00e73567cf9eadcfd5fae753ceefb0
uday <ualla@g.ucla.edu>
2016-06-28 21:15:08 +0200

    commit

--- o_X/BoardViewController.swift
+++ o_X/BoardViewController.swift
@@ -10,17 +10,34 @@ class BoardViewController: UIViewController {
     @IBOutlet weak var newGameButton: UIButton!
     // Create additional IBOutlets here.
     
+    @IBOutlet weak var LogoutButton: UIButton!
+    @IBOutlet weak var Button1: UIButton!
+    @IBOutlet weak var Button2: UIButton!
+    @IBOutlet weak var Button3: UIButton!
+    @IBOutlet weak var Button4: UIButton!
+    @IBOutlet weak var Button5: UIButton!
+    @IBOutlet weak var Button6: UIButton!
+    @IBOutlet weak var Button7: UIButton!
+    @IBOutlet weak var Button8: UIButton!
+    @IBOutlet weak var Button9: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    @IBAction func newGameButtonPressed(sender: UIButton) {
-        print("New game button pressed.")
-    }
     
+    @IBAction func LogOutPressed(sender: UIButton) {
+        print("Logged Out")
+    }
     // Create additional IBActions here.
+    @IBAction func ButtonPressed(sender: UIButton) {
+        print("Button \(sender.tag) pressed")
+    }
+    @IBAction func NewGamePressed(sender: UIButton) {
+        print("New Game Requested")
+    }
 
 }
 
